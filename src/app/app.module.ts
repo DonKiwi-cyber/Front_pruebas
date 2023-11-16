277e5ca12ad72b1e10642201cc1459fb74c0c0fe
DonKiwi-cyber <106345550+DonKiwi-cyber@users.noreply.github.com>
2023-11-16 12:02:39 -0600

    fix errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { A1Component } from './a1/a1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    A1Component
   ],
   imports: [
     BrowserModule
